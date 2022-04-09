690dedc23422f672fbd18e0cf0de64ff1c6cc468
giuspeppe9908 <66006287+giuspeppe9908@users.noreply.github.com>
2022-04-09 18:08:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+from tkinter.ttk import *
 from tkinter import messagebox
 import random as random
 from random import randint
 window = tk.Tk()
-window.title("Tris Game")
+window.title("Tiles Matching Game")
 matches = [1,1,2,2,3,3,4,4,5,5,6,6]
 random.shuffle(matches)
 #print(matches)
@@ -24,8 +25,9 @@ def something(b, number):
         if matches[answer_list[0]] == matches[answer_list[1]]:
             mylabel.config(text="MATCH!")
             for key in answer_dict:
-                key["fg"] = "green"
+                key["state"] = "disabled"
                 key["bg"] = "yellow"
+                key["fg"] = "blue"
             count = 0
             answer_list = []
             answer_dict = {}
@@ -33,7 +35,7 @@ def something(b, number):
             mylabel.config(text="DOH!")
             count = 0
             answer_list = []
-            messagebox.showinfo("Incorrect!","Incorrect")
+            messagebox.showerror("Incorrect!","Incorrect")
             for key in answer_dict:
                 key["text"] = " "
             answer_dict = {}
@@ -41,18 +43,18 @@ def something(b, number):
 my_frame = tk.Frame(window)
 my_frame.pack(pady=10)
 #define buttons
-b0 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b0, 0))
-b1 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b1, 1))
-b2 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b2, 2))
-b3 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b3, 3))
-b4 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b4, 4))
-b5 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b5, 5))
-b6 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b6, 6))
-b7 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b7, 7))
-b8 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b8, 8))
-b9 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b9, 9))
-b10 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b10, 10))
-b11 = tk.Button(my_frame, text=' ', font=("Helvetica", 20), height=3, width=6, command=lambda:something(b11, 11))
+b0 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b0, 0))
+b1 = tk.Button(my_frame, text=' ',fg="blue", font=("Helvetica", 20), height=3, width=6, command=lambda:something(b1, 1))
+b2 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b2, 2))
+b3 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b3, 3))
+b4 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b4, 4))
+b5 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b5, 5))
+b6 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b6, 6))
+b7 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b7, 7))
+b8 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b8, 8))
+b9 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b9, 9))
+b10 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b10, 10))
+b11 = tk.Button(my_frame, text=' ',fg="blue",font=("Helvetica", 20), height=3, width=6, command=lambda:something(b11, 11))
 #Grid buttons
 b0.grid(row=0, column=0)
 b1.grid(row=0, column=1)
@@ -68,4 +70,6 @@ b10.grid(row=2, column=2)
 b11.grid(row=2, column=3)
 mylabel = tk.Label(window, text="")
 mylabel.pack(pady=20)
+Exit = tk.Button(window,text="Exit Game", state="active", command=window.destroy)
+Exit.pack()
 window.mainloop()
